e8101d11f53650917507e73b5c4fbdce70f7ba31
humi53 <j.k.humi9@gmail.com>
2023-07-17 16:44:23 +0900

    2023-07-17 이미지 다중업로드

--- SpMVC_003_BBsV2/src/main/java/com/callor/bbs/controller/HomeController.java
+++ SpMVC_003_BBsV2/src/main/java/com/callor/bbs/controller/HomeController.java
@@ -1,20 +1,27 @@
 package com.callor.bbs.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.callor.bbs.config.QualifierConfig;
 import com.callor.bbs.dao.BBsDao;
+import com.callor.bbs.dao.FileDao;
 import com.callor.bbs.models.BBsDto;
-import com.callor.bbs.models.UserDto;
+import com.callor.bbs.models.FileDto;
 import com.callor.bbs.service.FileService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -38,13 +45,14 @@ public class HomeController {
 	
 	protected final FileService fileService;
 	private final BBsDao bbsDao;
-	
+	private final FileDao fileDao;
 	public HomeController(
 			@Qualifier(QualifierConfig.SERVICE.FILE_V2) FileService fileService,
+			FileDao fileDao,
 			BBsDao bbsDao) {
-		
 		this.fileService = fileService;
 		this.bbsDao = bbsDao;
+		this.fileDao = fileDao;
 	}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
@@ -73,41 +81,122 @@ public class HomeController {
 	 * UserDto userDto : input tag에 입력한 게시판관련 text 를 받는 곳
 	 * MultipartFile b_file : input tag 의 type="file" 에서 선택한 파일을 받는 곳
 	 */
-	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	@RequestMapping(value="/insert",method=RequestMethod.POST)
+	/*
+	 * 매개변수 설명
+	 * BBDto bbsDto
+	 * 	: input tag에 입력한 게시판 관련 text 를 받는 곳
+	 * MultipartFile b_file
+	 * 	: input tag 의 type="file" 에서 선택한 파일을 받는 곳
+	 */
 	public String insert(
-			BBsDto bbsDto,
-			@RequestParam(value = "b_file") MultipartFile b_file,
-			Model model) {
-		log.debug("사용자이름 : {}", bbsDto.getB_username());
+		BBsDto bbsDto,
+		// 싱글파일 업로드용
+		@RequestParam(value="b_file") MultipartFile b_file,
+		
+		// 멀티파일 업로드용
+		// @ReqeustParam 부착 금지 400 오류 발생
+		MultipartHttpServletRequest b_images,
+		
+		Model model
+		) {
+		
+		log.debug("사용자이름 : {}",bbsDto.getB_username());
 		log.debug(b_file.getOriginalFilename());
 		
 		String fileName = null;
+ 
 		try {
-			fileName = fileService.fileUp(b_file);
-			bbsDto.setB_image(fileName);
+			if( !b_file.getOriginalFilename().isEmpty() ) {
+				fileName = fileService.fileUp(b_file);
+				bbsDto.setB_image(fileName);
+			}
 			int result = bbsDao.insert(bbsDto);
+			log.debug("새로생성된 PK : {}", bbsDto.getB_seq());
+
+			// 멀티파일 업로드가 실행되었을때 만 filesUp() 호출
+			
+			// getFiles().size()
+			// b_image 를 List 로 변환하고 개수 세기
+			
+			// getFile().getSize(),
+			// 업로드한 파일의 개수가 담긴 변수값 참조
+			
+			if(b_images.getFile("b_images").getSize() > 0) {
+				List<FileDto> files = fileService.filesUp(b_images);
+				fileDao.insert(files, bbsDto.getB_seq());
+			}
 			return "redirect:/";
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			// e.printStackTrace();
-			log.debug("파일을 업로드 할 수 없음 오류 발생!!");
+			 e.printStackTrace();
+			log.debug("파일을 업로드 할수 없음 오류 발생!!");
 			return "redirect:/insert?error=FILE_UP_ERROR";
 		}
 		
 //		model.addAttribute("FILE_NAME",fileName);
 //		return "input";
+		
 	}
 	
 	@RequestMapping(value="/detail",method=RequestMethod.GET)
 	public String detail(String seq,Model model) {
 		
 		BBsDto bbsDto = bbsDao.findById(seq);
+		List<FileDto> files = fileDao.findByBSeq(bbsDto.getB_seq());
+		
 		model.addAttribute("BBS",bbsDto);
+		model.addAttribute("FILES", files);
+		
 		return "detail";
 		
 	}
 	
+	@RequestMapping(value = "/image_delete", method = RequestMethod.GET)
+	public String image_delete(String seq) {
+		
+		/*
+		 * 파일정보를 SELECT 하고
+		 * 물리적 파일을 삭제하고
+		 * 파일정보 데이터 삭제
+		 */
+		long seqNum = Long.valueOf(seq);
+		FileDto fileDto = fileDao.findByid(seqNum);
+		fileService.delete(fileDto.getI_uploadName());
+		int result = fileDao.delete(seqNum);
+		
+		return "redirect:/detail?seq=" + fileDto.getI_bseq();
+	}
 	
+	@ModelAttribute("BBS")
+	public BBsDto getBBsDto() {
+		BBsDto bbsDto = new BBsDto();
+		
+		Date date = new Date(System.currentTimeMillis());
+		Calendar calendar = Calendar.getInstance();
+
+		// 현재날짜와 시간 getter 하기
+		LocalDateTime localDateTime = LocalDateTime.now();
+		
+		// 날짜를 문자열로 변환하기 위한 pattern 생성
+		DateTimeFormatter dateFormatter 
+			= DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		// 시간을 문자열로 변환하기 위한 pattern 생성
+		DateTimeFormatter timeFormatter
+			= DateTimeFormatter.ofPattern("HH:mm:ss");
+	
+		// 날짜 형식의 데이터를 문자열로 변환
+		String strDate = localDateTime.format(dateFormatter);
+		// 시간 형식의 데이터를 문자열로 변환
+		String strTime = localDateTime.format(timeFormatter);
+	
+		bbsDto.setB_date(strDate);
+		bbsDto.setB_time(strTime);
+		bbsDto.setB_username("callor");
+		
+		return bbsDto;
+	}
 	
 	
 }
