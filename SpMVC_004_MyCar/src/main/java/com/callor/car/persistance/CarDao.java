928be04a85e7c70c8e935eabc151198c86da9aa3
humi53 <j.k.humi9@gmail.com>
2023-07-18 16:16:55 +0900

    myCar 기본설정부터 ajax까지

---
+++ SpMVC_004_MyCar/src/main/java/com/callor/car/persistance/CarDao.java
@@ -0,0 +1,10 @@
+package com.callor.car.persistance;
+
+import com.callor.car.model.CarDto;
+
+public interface CarDao {
+	
+	public int insert(CarDto carDto);
+	
+	public CarDto findTachoByCarNum(String carnum);
+}
