9340e310bc8656d4e259e0b70093a9c823f34ae8
humi53 <j.k.humi9@gmail.com>
2023-07-20 16:02:58 +0900

    로그인아웃 2023-07-20

--- SpMVC_004_MyCarV2/src/main/java/com/callor/car/service/impl/CarServiceImplV1.java
+++ SpMVC_004_MyCarV2/src/main/java/com/callor/car/service/impl/CarServiceImplV1.java
@@ -2,6 +2,7 @@ package com.callor.car.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.callor.car.model.CarDto;
@@ -33,6 +34,16 @@ public class CarServiceImplV1 implements CarService{
 	@Override
 	public List<CarDto> selectAll() {
 		return carDao.selectAll();
+	}
+	
+	
+	/*
+	 * 프로젝트가 시작될때
+	 * method 에 매개변수를 자동 주입해 달라 라는 요청
+	 */
+	@Autowired
+	public void create_table() {
+		carDao.create_carm_table(null);
 	}
 	
 }
